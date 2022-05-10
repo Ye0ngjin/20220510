b37217550d4f77733243f5e4357d5d703e906ba1
yj <wkgek@naver.com>
2022-05-10 11:31:05 +0900

    정렬

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main()
 {re:
 	int A = 0;
-	cout << "1부터 10까지의 정수를 입력하시오. "; cin >> A;
+	cout << "입력하고 싶은 개수만큼의 정수를 입력하시오(1~10). "; cin >> A;
 
 
-	if (A < 0)
+	if (A < 1)
+	{
+		goto re;
+	}
+	else if (A > 10)
 	{
 		goto re;
 	}
@@ -17,23 +21,47 @@ int main()
 
 	for (int i = 0; i < A; i++)
 	{
-		cout << (i + 1) << "번째 숫자를 입력하시오.";  cin >> Array[i];
+		cout << (i + 1) << "번째 정수를 입력하시오.";  cin >> Array[i];
 	}
 
-	int total = 0;
+	int Add = 0;
 
-	int* ptr_total;
-
-	ptr_total = &total;
 
 	for (int i = 0; i < A; i++)
 
 	{
-		total += Array[i];
+		Add += Array[i];
 	}
 
-	cout << "입력한 정수들의 합은 " << *ptr_total << endl;
+	cout << "입력한 정수들의 합은 : " << Add << endl;
+
+	int temp = 0;
+	for (int i = 1; i < A; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (Array[i] < Array[j])
+			{
+				temp = Array[i];
+				for (int k = i - 1; k >= j; k--)
+				{
+					Array[k + 1] = Array[k];
+				}
+				Array[j] = temp;
+			}
+		}
+	}
+
+	cout << "입력한 숫자들의 정렬 : ";
+
+	for (int i = 0; i < A; i++)
+	{
+		cout << Array[i] << ", ";
+	}
+
+	cout << endl;
 
 	delete[] Array;
+
 	return 0;
 }
